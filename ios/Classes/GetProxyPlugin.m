47e9ca4d2419dbd639bb06993ba8a36faff68c7b
user177936 <user177936@EU210.local>
2020-10-30 15:25:33 +0000

    First attempt to get IOS version working

--- ios/Classes/GetProxyPlugin.m
+++ ios/Classes/GetProxyPlugin.m
@@ -10,32 +10,37 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"getProxy" isEqualToString:call.method]) {
+    result([self getProxy]);
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
-std::string getProxy()
-{
+- (NSString*) getProxy {
 
-    CFDictionaryRef dicRef = CFNetworkCopySystemProxySettings();
-    const CFStringRef proxyCFstr = (const CFStringRef)CFDictionaryGetValue(dicRef, (const void*)kCFNetworkProxiesHTTPProxy);
-    const CFNumberRef portCFnum = (const CFNumberRef)CFDictionaryGetValue(dicRef, (const void*)kCFNetworkProxiesHTTPPort);
-    char buffer[4096];
-    memset(buffer, 0, 4096);
-    SInt32 port;
-    if (CFStringGetCString(proxyCFstr, buffer, 4096, kCFStringEncodingUTF8))
-    {
-      if (CFNumberGetValue(portCFnum, kCFNumberSInt32Type, &port))
+    @try {
+      CFDictionaryRef dicRef = CFNetworkCopySystemProxySettings();
+      const CFStringRef proxyCFstr = (const CFStringRef)CFDictionaryGetValue(dicRef, (const void*)kCFNetworkProxiesHTTPProxy);
+      const CFNumberRef portCFnum = (const CFNumberRef)CFDictionaryGetValue(dicRef, (const void*)kCFNetworkProxiesHTTPPort);
+      char buffer[4096];
+      memset(buffer, 0, 4096);
+      SInt32 port;
+      if (CFStringGetCString(proxyCFstr, buffer, 4096, kCFStringEncodingUTF8))
       {
-        return [stringWithFormat: @"PROXY %s:%d", std::string(buffer), port];
-      } else {
-        return [stringWithFormat: @"PROXY %s", std::string(buffer)];
+        if (CFNumberGetValue(portCFnum, kCFNumberSInt32Type, &port))
+        {
+          return [NSString stringWithFormat: @"PROXY %s:%d", buffer, port];
+        } else {
+          return [NSString stringWithFormat: @"PROXY %s", buffer];
+        }
       }
+      return @"";
     }
-    return "";
+
+    @catch (id ue) {
+      return @"";
+    }
 }
 
 
